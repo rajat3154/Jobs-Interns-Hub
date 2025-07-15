00860cbc401c3bb00fcc072fe191af3ac47fc07d
rajat3154 <rajaturanvir31@gmail.com>
2025-07-15 20:17:03 +0530

    Jobs-Interns-Hub

--- frontend/src/components/CategoryCarousel.jsx
+++ frontend/src/components/CategoryCarousel.jsx
@@ -20,21 +20,21 @@ const category = [
 const CategoryCarousel = ({ setQuery }) => {
   return (
     <div className="bg-black text-white text-center ">
-      <div className="container mx-auto text-center px-2 sm:px-4">
+      <div className="container mx-auto text-center px-1 sm:px-4">
         <h2 className="text-2xl sm:text-3xl font-bold mb-6 sm:mb-8">Browse by Category</h2>
 
-        <div className="w-full">
+        <div className="w-full overflow-x-auto">
           <Carousel className="w-full max-w-full sm:max-w-3xl mx-auto">
             <CarouselContent>
               {category.map((cat, index) => (
                 <CarouselItem
                   key={index}
-                  className="basis-full xs:basis-2/3 sm:basis-1/2 lg:basis-1/3 px-1 sm:px-3 py-2 sm:py-4"
+                  className="w-full box-border px-2 py-2 sm:px-3 sm:py-4"
                 >
                   <Button
                     variant="outline"
                     onClick={() => setQuery(cat)}
-                    className="w-full py-2 sm:py-3 px-4 sm:px-6 text-base sm:text-lg rounded-full border-2 border-blue-500 text-white bg-black hover:bg-blue-500 hover:text-white transition-all cursor-pointer"
+                    className="w-full py-2 sm:py-3 px-2 sm:px-6 text-base sm:text-lg rounded-full border-2 border-blue-500 text-white bg-black hover:bg-blue-500 hover:text-white transition-all cursor-pointer"
                   >
                     {cat}
                   </Button>
